9f6cb43d7b5795e39826a04fba0b23a15cf07755
leeendl <127639272+leeendl@users.noreply.github.com>
2026-04-15 16:36:00 -0500

    add memcpy support for overwrite

--- myblob/myblob.c
+++ myblob/myblob.c
@@ -11,19 +11,19 @@ struct myblob
         mSize = len;
         mData = new char[mSize];
 
-        _copy(mData, _data, mSize);
+        _memcpy(mData, _data, mSize);
     }
     myblob(int _x)
     {
         mSize = sizeof(int);
         mData = new char[mSize];
 
-        _copy(this->mData, &_x, mSize);
+        _memcpy(mData, &_x, mSize);
     }
 
-    void copy_to(void* _dest, int destSize) const
+    void copy_to(void* _dest, int _size) const
     {
-        _copy(_dest, this->mData, destSize);
+        _memcpy(_dest, mData, _size);
     }
 
     const char* data() const { return this->mData; }
@@ -35,8 +35,11 @@ private:
     char* mData = nullptr;
     int   mSize = 0;
 
-    static void _copy(void* _dest, const void* _data, int _size)
+    static void _memcpy(void* _dest, const void* _data, int _size)
     {
+#if defined(_STRING_H) || defined(_INC_STRING) || defined(_GLIBCXX_STRING_H)
+        std::memcpy(_dest, _data, _size); // for memory overwrite like mimalloc
+#else
         char*       dest = (char*)_dest;
         const char* data = (const char*)_data;
         
@@ -44,5 +47,6 @@ private:
         {
             dest[i] = data[i];
         }
+#endif
     }
 };
